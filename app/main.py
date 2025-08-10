4285433b7e5d276df8804bc8c5b83bdb99ee80f2
potatocoder2418 <cuteamor1824@gmail.com>
2025-08-10 22:38:09 +0530

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware import Middleware
-from starlette.middleware.base import BaseHTTPMiddleware
 from app.routes import auth
 from app.core.config import settings
-
-# Create middleware for CORS headers
-class CustomCORSMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        response = await call_next(request)
-        response.headers['Access-Control-Allow-Origin'] = 'https://hackodisha.vercel.app'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        return response
 
 app = FastAPI()
 
@@ -22,13 +10,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://hackodisha.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"]
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
+    expose_headers=["*"],
+    max_age=600  # Cache preflight request for 10 minutes
 )
-
-# Add custom CORS middleware
-app.add_middleware(CustomCORSMiddleware)
 
 app.include_router(auth.router)
 
